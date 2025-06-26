56715da01994a9fe0608a43dcc0f8b7dd3cbc203
KarandeepSingh258255 <karandeepkrish@gmail.com>
2025-06-26 16:55:22 -0400

    a

---
+++ script2.js
@@ -0,0 +1,19 @@
+function gradeStudent() {
+    let score = Number(document.getElementById('scoreInput').value);
+
+    if (score >= 90){
+        alert("Grade: A");
+        console.log("Grade: A")
+    } else if (score >= 80){
+        alert("Grade: B");
+        console.log("Grade: B")
+    } else if (score >= 70) {
+        alert("Grade: C");
+        console.log("Grade: C")
+    } else {
+        alert("Grade: Failed");
+        console.log("Grade: Failed");
+    }
+
+
+}
